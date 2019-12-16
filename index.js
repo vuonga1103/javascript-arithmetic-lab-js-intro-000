b2d1e9db0bd1c5ede045fafaa6d4586d63a52ffa
Anh Vuong <vuonga1103@yahoo.com>
2019-12-16 02:35:46 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,32 +1,32 @@
 function add() {
-  
+
 }
 
 function subtract() {
-  
+
 }
 
 function multiply() {
-  
+
 }
 
 function divide() {
-  
+
 }
 
 function inc(n) {
-  
+
 }
 
 function dec(n) {
 
-  
+
 }
 
 function makeInt(n) {
-  
+
 }
 
 function preserveDecimal(n) {
-  
+
 }
