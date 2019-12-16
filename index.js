01a9ac065a0c3f8e7a9e3e16769e767d1a39fcc7
Anh Vuong <vuonga1103@yahoo.com>
2019-12-16 02:40:44 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -7,11 +7,11 @@ function subtract(a, b) {
 }
 
 function multiply(a, b) {
-  return a - b;
+  return a * b;
 }
 
 function divide(a, b) {
-  return a - b
+  return a / b
 }
 
 function inc(n) {
@@ -23,9 +23,9 @@ function dec(n) {
 }
 
 function makeInt(n) {
-  parseInt(n)
+  return parseInt(n)
 }
 
 function preserveDecimal(n) {
-  parseGloat(n)
+  return parseFloat(n)
 }
